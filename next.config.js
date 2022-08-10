36af835a5d24d0567f58f878dee318f634044b45
joshpainter <josh@joshpainter.com>
2022-08-10 11:41:53 -0500

    update routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/go4me0001.png',
+        has: [
+          {
+            type: 'host',
+            value: '(?<pfpname>.*)\.localhost|\.go4\.me'
+          }
+        ],
+        destination: 'https://go4me-pfps.s3.amazonaws.com/go4me0001-:pfpname.png',
+      },
+      {
+        source: '/domain.json',
+        has: [
+          {
+            type: 'host',
+            value: '(?<pfpname>.*)\.localhost|\.go4\.me'
+          }
+        ],        
+        destination: 'https://go4me-domains.s3.amazonaws.com/:pfpname.json',
+      },      
+    ]
+  },
 }
 
 module.exports = nextConfig
