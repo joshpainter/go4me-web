c884c5ee3bae7e0fe2148c5696c124a8c9de9719
joshpainter <josh@joshpainter.com>
2025-08-20 19:55:58 -0500

    added target price explanation

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -181,9 +181,16 @@ export default function HowItWorks({ rootHostForLinks }) {
           </section>
 
           <section style={{ marginTop: 24}}>
+            <h3>Maintaining the $G4M Target Price</h3>
+            <p>
+              The target price for $G4M is 10,000 per 1 XCH. If the price of $G4M is 11,000 per 1 XCH or higher, the system will automatically generate all new offers in $G4M tokens until the target price is below 11,000 per 1 XCH again.
+            </p>
+          </section>
+
+          <section style={{ marginTop: 24}}>
             <h3>Expiring Offers and Decreasing Prices</h3>
             <p>
-              If a PFP offer for XCH expires after 7 days, it will be regenerated as a $G4M offer. If that offer also expires after another 7 days, it will be regenerated as a slightly cheaper $G4M offer. It will continue decreasing in price every 7 days until sold or until it reaches 200 $G4M, at which point it will not go lower.
+              If a PFP offer for XCH expires after 7 days, it will be regenerated as a $G4M offer. If that offer also expires after another 7 days, it will be regenerated as a slightly cheaper $G4M offer. It will continue decreasing in price every 7 days until sold or until it reaches 200 $G4M, at which point it will not go lower. The price of the $G4M offer will be calculated based on the current market price according to TibetSwap.
             </p>
             <p>
               Once sold, the next edition will be generated and offered for the normal XCH price again for 7 days, and the cycle will repeat. And don&apos;t forget: you still get 10% royalty on every sale, including $G4M tokens!
